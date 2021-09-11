64898a4a9a7133ee5fd2cdc8eccf25bf87f63aa5
cluster05 <ajayakumbhar1234@gmail.com>
2021-09-11 11:43:42 +0530

    document structure added

---
+++ src/document/document.controller.ts
@@ -0,0 +1,43 @@
+import {
+    Controller,
+    Get,
+    Param,
+    Post,
+    Res,
+    UploadedFile,
+    UseInterceptors,
+  } from '@nestjs/common';
+  import { FileInterceptor } from '@nestjs/platform-express';
+  import { diskStorage } from 'multer';
+  import { join } from 'path';
+  import { v4 as uuidv4 } from 'uuid';
+
+@Controller('document')
+export class DocumentController {
+
+    @Post()
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: join(__dirname, '../documents/'),
+        filename: (req, file, cb) => {
+          const fileSplit = file.originalname.split('.');
+          const fileExt = fileSplit[fileSplit.length - 1];
+          const filename = uuidv4() + '.' + fileExt;
+          cb(null, filename);
+        },
+      }),
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return {
+      url: "http://localhost:3000/upload/documents/" + file.filename,
+    };
+  }
+
+  @Get("/documents/:document")
+  getFile(@Param('document') document: string, @Res() res){
+    return res.sendFile(join(__dirname, '../documents/' + document));
+  }
+
+}
