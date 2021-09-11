eb755751dc4d37a61e03daf604f681478526975d
cluster05 <ajayakumbhar1234@gmail.com>
2021-09-11 10:45:49 +0530

    import module convert and upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UplaodModule } from './uplaod/uplaod.module';
+import { ConvertModule } from './convert/convert.module';
 
 @Module({
-  imports: [],
+  imports: [UplaodModule, ConvertModule],
   controllers: [],
   providers: [],
 })
