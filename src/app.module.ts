18e3ae3ac3caef08daf2cda46e1cfa59e4359fdc
cluster05 <ajayakumbhar1234@gmail.com>
2021-09-11 12:27:12 +0530

    remove convert module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConvertModule } from './convert/convert.module';
 import { DocumentModule } from './document/document.module';
 
 @Module({
-  imports: [ConvertModule, DocumentModule],
+  imports: [DocumentModule],
   controllers: [],
   providers: [],
 })
