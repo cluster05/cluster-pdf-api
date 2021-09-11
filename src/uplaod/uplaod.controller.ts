907a56eb44e3185f5d41a5a9734d32401daae8bd
cluster05 <ajayakumbhar1234@gmail.com>
2021-09-11 11:34:49 +0530

    upalod file get that file done.

--- src/uplaod/uplaod.controller.ts
+++ src/uplaod/uplaod.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { join } from 'path';
+import { v4 as uuidv4 } from 'uuid';
 
-@Controller('uplaod')
-export class UplaodController {}
+@Controller('upload')
+export class UplaodController {
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: join(__dirname, '../documents/'),
+        filename: (req, file, cb) => {
+          const fileSplit = file.originalname.split('.');
+          const fileExt = fileSplit[fileSplit.length - 1];
+          const filename = uuidv4() + '.' + fileExt;
+          cb(null, filename);
+        },
+      }),
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return {
+      url: 'http://localhost:3000/upload/documents/' + file.filename,
+    };
+  }
+
+  @Get("/documents/:document")
+  getFile(@Param('document') document: string, @Res() res){
+    return res.sendFile(join(__dirname, '../documents/' + document));
+  }
+}
