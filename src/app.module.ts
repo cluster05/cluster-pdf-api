645fa45ac7a0583e608b1db5c4af1543f08cdbff
cluster05 <ajayakumbhar1234@gmail.com>
2021-09-11 11:51:58 +0530

    upload file with document controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UplaodModule } from './uplaod/uplaod.module';
 import { ConvertModule } from './convert/convert.module';
 import { DocumentModule } from './document/document.module';
 
 @Module({
-  imports: [UplaodModule, ConvertModule, DocumentModule],
+  imports: [ConvertModule, DocumentModule],
   controllers: [],
   providers: [],
 })
