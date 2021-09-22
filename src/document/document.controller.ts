8b69e4145a3efc11818617c98955b72176b9336f
cluster05 <ajayakumbhar1234@gmail.com>
2021-09-22 21:01:25 +0530

    upload file without aws [ local storage]

--- src/document/document.controller.ts
+++ src/document/document.controller.ts
@@ -9,20 +9,26 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 import { join } from 'path';
+import { v4 as uuidv4 } from 'uuid';
 import { ConvertDTO } from './dto/convert.dto';
 import { DocumentService } from './document.service';
 import { MergeDTO } from './dto/merge.dto';
-
 @Controller('document')
 export class DocumentController {
   constructor(private documentService: DocumentService) { }
 
   @Post('/upload')
   @UseInterceptors(FileInterceptor('file'))
-  async uplaod(@UploadedFile() file: Express.Multer.File) {
-      return await this.documentService.upload(file);
+  async upload(@UploadedFile() file: Express.Multer.File) {
+    return await this.documentService.upload(file);
   }
+
+  @Get('/:document')
+  getFile(@Param('document') document: string, @Res() res) {
+    return res.sendFile(join(__dirname, '../documents/' + document));
+  }  
   
   @Post('/convert')
   convert(@Body() convertDTO: ConvertDTO) {
@@ -37,12 +43,6 @@ export class DocumentController {
   @Post('/compess')
   compress(){
     return this.documentService.compress();
-  }
-
-  @Post()
-  @UseInterceptors(FileInterceptor('file'))
-  async upload(@UploadedFile() file: Express.Multer.File) {
-    return this.documentService.upload(file);
   }
 
 }
