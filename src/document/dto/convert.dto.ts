02c0625fc262c9d8658bc5f6f4c6ba31ce1023ae
cluster05 <ajayakumbhar1234@gmail.com>
2021-10-09 23:32:49 +0530

    from and to images type added

--- src/document/dto/convert.dto.ts
+++ src/document/dto/convert.dto.ts
@@ -4,22 +4,22 @@ export class ConvertDTO {
 
   @IsString()
   @IsNotEmpty()
-  @IsIn(['office','pdf'])
+  @IsIn(['office','pdf','image'])
   from: string;
 
   @IsString()
   @IsNotEmpty()
-  @IsIn(['office','pdf'])
+  @IsIn(['office','pdf','image'])
   to: string;
 
   @IsString()
   @IsNotEmpty()
-  @IsIn(['doc','docx','xlx','xlsx','ppt','png','pdf'])
+  @IsIn(['doc','docx','xlx','xlsx','ppt','png','jpeg','pdf'])
   fromType : string;
 
   @IsString()
   @IsNotEmpty()
-  @IsIn(['doc','docx','xlx','xlsx','ppt','png','pdf'])
+  @IsIn(['doc','docx','xlx','xlsx','ppt','png','jpeg','pdf'])
   toType :string;
 
   pages : number | number[]
