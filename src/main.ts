54b28497d9678cc2e0119c2e8c66350520644c49
cluster05 <ajayakumbhar1234@gmail.com>
2021-10-10 12:26:30 +0530

    Added logger for PORT running

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 import fetch from 'node-fetch';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 if (!globalThis.fetch) {
   globalThis.fetch = fetch;
 }
 
+const PORT = process.env.PORT || 8000;
+const logger = new Logger('Application');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(8080);
+  logger.log('Application Running on PORT'+PORT);  
+  await app.listen(PORT);
 }
 bootstrap();
