beb19fea05d2aae78be682ac70c149ad02198763
cluster05 <ajayakumbhar1234@gmail.com>
2021-10-12 22:49:50 +0530

    schema for new fields opration and isDeleted

--- src/document/schema/document.schema.ts
+++ src/document/schema/document.schema.ts
@@ -1,31 +1,39 @@
 import { Schema } from 'mongoose';
 
 export const DocumentSchema = new Schema({
-    keys :{
-        type: [String],
-        required: true    
-    },
-    timestamp :{
-        type : Number,
-        required:true
-    },
-    oprationStart:{
-        type : Number,
-        required:true
-    },
-    oprationEnd:{
-        type : Number,
-    },
-    failedReason:{
-        type : String,
-        default:'',
-    },
-    date :{
-        type: Date, 
-        required:true,
-    },
-    ipInfo :{
-        type : Object,
-        required:true,
-    }
-}) 
+  keys: {
+    type: [String],
+    required: true,
+  },
+  timestamp: {
+    type: Number,
+    required: true,
+  },
+  oprationStart: {
+    type: Number,
+    required: true,
+  },
+  oprationEnd: {
+    type: Number,
+  },
+  failedReason: {
+    type: String,
+    default: '',
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  ipInfo: {
+    type: Object,
+    required: true,
+  },
+  opration: {
+    type: String,
+    default: 'DEFAULT',
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+});
