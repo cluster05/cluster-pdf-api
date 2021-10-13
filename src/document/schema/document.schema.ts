2626f2d911c2f7732b537a45ad879dac12588035
cluster05 <ajayakumbhar1234@gmail.com>
2021-10-13 20:22:42 +0530

    version key removed

--- src/document/schema/document.schema.ts
+++ src/document/schema/document.schema.ts
@@ -1,39 +1,42 @@
 import { Schema } from 'mongoose';
 
-export const DocumentSchema = new Schema({
-  keys: {
-    type: [String],
-    required: true,
+export const DocumentSchema = new Schema(
+  {
+    keys: {
+      type: [String],
+      required: true,
+    },
+    timestamp: {
+      type: Number,
+      required: true,
+    },
+    oprationStart: {
+      type: Number,
+      required: true,
+    },
+    oprationEnd: {
+      type: Number,
+    },
+    failedReason: {
+      type: String,
+      default: '',
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    ipInfo: {
+      type: Object,
+      required: true,
+    },
+    opration: {
+      type: String,
+      default: 'DEFAULT',
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
   },
-  timestamp: {
-    type: Number,
-    required: true,
-  },
-  oprationStart: {
-    type: Number,
-    required: true,
-  },
-  oprationEnd: {
-    type: Number,
-  },
-  failedReason: {
-    type: String,
-    default: '',
-  },
-  date: {
-    type: Date,
-    required: true,
-  },
-  ipInfo: {
-    type: Object,
-    required: true,
-  },
-  opration: {
-    type: String,
-    default: 'DEFAULT',
-  },
-  isDeleted: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { versionKey: false },
+);
