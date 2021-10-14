7aca39e3bf2954682b9b661cfccf07a5bba58216
cluster05 <ajayakumbhar1234@gmail.com>
2021-10-14 23:49:25 +0530

    mongoose module added to analytics

--- src/analytics/analytics.module.ts
+++ src/analytics/analytics.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DocumentSchema } from 'src/document/schema/document.schema';
 import { AnalyticsController } from './analytics.controller';
 import { AnalyticsService } from './analytics.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Documents', schema: DocumentSchema }]),
+  ],
   controllers: [AnalyticsController],
-  providers: [AnalyticsService]
+  providers: [AnalyticsService],
 })
 export class AnalyticsModule {}
