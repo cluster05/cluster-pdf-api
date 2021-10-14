a679b3e21011eab40ddcab0726e8b0f51e0d1d43
cluster05 <ajayakumbhar1234@gmail.com>
2021-10-14 17:44:18 +0530

    analytics for counter query added

--- src/analytics/analytics.service.ts
+++ src/analytics/analytics.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AnalyticsService {}
+export class AnalyticsService {
+  /* 
+    COUNTER FOR PROCESSING 
+    db.documents.aggregate([
+        { 
+            $group : { 
+                _id : "$opration",
+                count: { $sum: 1 }
+            } 
+        },
+        {
+            $project: { opration: '$_id' , count : "$count" , _id:0},
+        },
+    ])
+
+    
+    */
+}
