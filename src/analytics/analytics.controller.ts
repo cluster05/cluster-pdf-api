fed3168edede309804125d44880dac8a9afb6fc0
cluster05 <ajayakumbhar1234@gmail.com>
2021-10-15 11:00:24 +0530

    done analytics

--- src/analytics/analytics.controller.ts
+++ src/analytics/analytics.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { AnalyticsService } from './analytics.service';
 
 @Controller('analytics')
-export class AnalyticsController {}
+export class AnalyticsController {
+  constructor(private analyticsService: AnalyticsService) {}
+
+  @Get('/year')
+  async year() {
+    return await this.analyticsService.year();
+  }
+
+  @Get('/full')
+  async analytics() {
+    return await this.analyticsService.analytics();
+  }
+
+  @Get('/opration')
+  async opration() {
+    return await this.analyticsService.oprationAnalytics();
+  }
+}
--- src/analytics/analytics.service.ts
+++ src/analytics/analytics.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { DocumentModel } from 'src/document/model/document.model';
@@ -9,14 +9,124 @@ export class AnalyticsService {
     @InjectModel('Documents') private documentModel: Model<DocumentModel>,
   ) {}
 
-  async core(year: number, opration : string) {
+  async analytics() {
+    try {
+      const analytic = {
+        y: await this.year(),
+        w: await this._weeks(),
+        m: await this._months(),
+        d: await this._dayOfMonth(),
+        h: await this._hour(),
+        isDeleted: await this.isDeleted(),
+        failed: await this.failed(),
+      };
+      return analytic;
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(
+        'Error in getting analytics data',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
 
-    const builder = { `` : "$date" };
-    
-    const response = await this.documentModel.aggregate([
+  private async failed() {
+    return await this.documentModel.aggregate([
+      {
+        $match: { failedReason: { $exists: true, $ne: '' } },
+      },
+    ]);
+  }
+
+  private async isDeleted() {
+    return await this.documentModel.aggregate([
       {
         $group: {
-          _id: { builder : '$date' },
+          _id: '$isDeleted',
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $project: { isDeleted: '$_id', count: '$count', _id: 0 },
+      },
+    ]);
+  }
+
+  async year() {
+    return await this.documentModel.aggregate([
+      {
+        $group: {
+          _id: { $year: '$date' },
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $project: { year: '$_id', count: '$count', _id: 0 },
+      },
+    ]);
+  }
+
+  private async _weeks() {
+    return await this.documentModel.aggregate([
+      {
+        $group: {
+          _id: { $week: '$date' },
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $project: { week: '$_id', count: '$count', _id: 0 },
+      },
+    ]);
+  }
+
+  private async _months() {
+    return await this.documentModel.aggregate([
+      {
+        $group: {
+          _id: { $month: '$date' },
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $project: { month: '$_id', count: '$count', _id: 0 },
+      },
+    ]);
+  }
+
+  private async _dayOfMonth() {
+    return await this.documentModel.aggregate([
+      {
+        $group: {
+          _id: { $dayOfMonth: '$date' },
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $project: { day: '$_id', count: '$count', _id: 0 },
+      },
+    ]);
+  }
+
+  private async _hour() {
+    return await this.documentModel.aggregate([
+      {
+        $group: {
+          _id: { $hour: '$date' },
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $project: { hour: '$_id', count: '$count', _id: 0 },
+      },
+    ]);
+  }
+
+  async oprationAnalytics() {
+    return await this.documentModel.aggregate([
+      {
+        $group: {
+          _id: '$opration',
           count: { $sum: 1 },
         },
       },
@@ -24,119 +134,5 @@ export class AnalyticsService {
         $project: { opration: '$_id', count: '$count', _id: 0 },
       },
     ]);
-
-    return response
   }
-
-  async analytics(year: number) {
-
-  }
-
-  /* 
-    COUNTER FOR PROCESSING 
-    db.documents.aggregate([
-        { 
-            $group : { 
-                _id : "$opration",
-                count: { $sum: 1 }
-            } 
-        },
-        {
-            $project: { opration: '$_id' , count : "$count" , _id:0},
-        },
-    ])
-
-    
-    */
 }
-
-
-// ----------------------------------------------
-
-db.documents.aggregate([
-    {
-        $group: {
-            _id: {$year: "$date"}, 
-            count   : {$sum: 1} 
-        },
-    },
-    {
-        $project: { year: '$_id' , count : "$count" , _id:0},
-    },
-])
-
-// ----------------------------------------------
-
-db.documents.aggregate([
-    {
-        $group: {
-            _id: {$week: "$date"}, 
-            count   : {$sum: 1} 
-        },
-    },
-    {
-        $project: { week: '$_id' , count : "$count" , _id:0},
-    },
-])
-
-// ----------------------------------------------------
-
-
-db.documents.aggregate([
-    {
-        $group: {
-            _id: { $month: "$date"}, 
-            count   : {$sum: 1} 
-        },
-    },
-    {
-        $project: {  month: '$_id' , count : "$count" , _id:0},
-    },
-])
-
-
-// ----------------------------------------------------
-
-
-db.documents.aggregate([
-    {
-        $group: {
-            _id: {$dayOfMonth: "$date"}, 
-            count   : {$sum: 1} 
-        },
-    },
-    {
-        $project: { day: '$_id' , count : "$count" , _id:0},
-    },
-])
-
-// ----------------------------------------------------
-
-
-db.documents.aggregate([
-    {
-        $group: {
-            _id: {$hour: "$date"}, 
-            count   : {$sum: 1} 
-        },
-    },
-    {
-        $project: { hour: '$_id' , count : "$count" , _id:0},
-    },
-])
-
-
-// ----------------------------------------------------
-
-
-db.documents.aggregate([
-    {
-        $group: {
-            _id: {$minute: "$date"}, 
-            count   : {$sum: 1} 
-        },
-    },
-    {
-        $project: { minute: '$_id' , count : "$count" , _id:0},
-    },
-])
